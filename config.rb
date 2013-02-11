288c17713301e33fdfc779fb185656eb28cb2ef5
Nathan Henderson <nathos@nathos.com>
2013-02-11 18:44:51 -0500

    removed 960.gs from the config file

--- config.rb
+++ config.rb
@@ -1,13 +1,9 @@
-### 
+###
 # Compass
 ###
 
 # Susy grids in Compass
 require 'susy'
-
-# 960.gs grids in Compass
-# First: gem install compass-960-plugin
-# require 'ninesixty'
 
 # Change Compass configuration
 # compass_config do |config|
@@ -34,13 +30,13 @@ require 'susy'
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -81,21 +77,21 @@ end
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
-  
+
   # Minify Javascript on build
   # activate :minify_javascript
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
