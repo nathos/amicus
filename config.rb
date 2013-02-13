72aebc558b938b7a722dcbf3dc98c7e041a19fdd
Nathan Henderson <nathos@nathos.com>
2013-02-13 14:46:34 -0500

    added support for custom text in the img_holder helper method

--- config.rb
+++ config.rb
@@ -68,9 +68,10 @@ helpers do
     return "" unless opts[:width] && opts[:height]
     return "" unless opts[:width].to_s =~ /^\d+$/ && opts[:height].to_s =~ /^\d+$/
 
-    img = "<img data-src='holder.js/#{opts[:width]}x#{opts[:height]}"
-    img = "#{img}/#{opts[:bgcolor]}:#{opts[:fgcolor]}" if opts[:fgcolor] && opts[:bgcolor]
-    img = "#{img}'>"
+    img =  "<img data-src=\"holder.js/#{opts[:width]}x#{opts[:height]}"
+    img += "/#{opts[:bgcolor]}:#{opts[:fgcolor]}" if opts[:fgcolor] && opts[:bgcolor]
+    img += "/text:#{opts[:text].gsub(/'/,"\'")}" if opts[:text]
+    img += "\">"
 
     img
   end
